1793485367819e7792105a6565dba292b174abb6
c-moli <carlos.molinero90@gmail.com>
2020-06-16 15:46:57 +0200

    curso

--- scripts. PY/tema4. Plot/practicar_matplot.py
+++ scripts. PY/tema4. Plot/practicar_matplot.py
@@ -10,5 +10,11 @@ plt.axis([0,10,0,10]) # 4 números. Lso dos primeros para eje x; otros dos eje y
 
 data = np.arange(0.0, 10.0, 0.2)
 plt.plot(data,data,"b--")
+plt.show()
 
+plt.figure(1)
+plt.subplot(2,1,1)
+plt.plot([1,3,5,7])
+plt.subplot(2,1,2) #sin esta instrucción se harían las dos rectas en la misma gráfica
+plt.plot([7,5,3,1])
 plt.show()
