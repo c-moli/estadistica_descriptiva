a1cb47e37d91f3b79e1ed3abd875ad1ee0c453a7
c-moli <carlos.molinero90@gmail.com>
2020-06-17 16:18:00 +0200

    curso

--- ejercicios/Tema4.Plot/ejercicio3.py
+++ ejercicios/Tema4.Plot/ejercicio3.py
@@ -1,2 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+n = 12
+X = np.arange(n)
+y1 = (1 - X / float(n)) * np.random.uniform(0.5, 1.0, n)
+y2 = (1 - X / float(n)) * np.random.uniform(0.5, 1.0, n) 
+
+plt.bar(X, +y1, facecolor='#9999ff', edgecolor='white')
+plt.bar(X, -y2, facecolor='#ff9999', edgecolor='white')
+
+for x, y in zip(X, y1):     
+    plt.text(x, y + 0.03, '%.2f' % y, ha='center', va='bottom', fontdict={"size":9, "stretch":0})
+
+for x, y in zip(X, y2):     
+    plt.text(x, -y-0.15, '%.2f' % y, ha='center', va='bottom', fontdict={"size":9, "stretch":0})
+
+plt.xticks([])
+plt.yticks([])
+plt.ylim(-1.25, +1.25)
+
+plt.show()
--- ejercicios/Tema4.Plot/ejercicio4.py
+++ ejercicios/Tema4.Plot/ejercicio4.py
@@ -1,2 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+Z = np.ones(20)
+Z[-1] = 2 #para que el último quesito sea más grande
+
+queso = np.array([0.04]*20) #creo un array indicando la separación de cada quesito
+queso[-1]=0.2 # para que el último quesito esté separado más distancia
+
+plt.pie(Z, colors=['%f' % (i/len(Z)) for i in range(len(Z))], explode=queso) #el blucle quiere decir que por cada quesito, el color va a ser igual al resultado de dividir la posición del bucle entre 20. 0 es negro y 1 es blanco. De esta forma dividimos la distancia entre 0 y 1 en 20 partes que corresponden a los 20 quesitos y cada uno coge su matiz del color (0, 0.05 etc)
+
+plt.xticks([])
+plt.yticks([])
+
+plt.show()
